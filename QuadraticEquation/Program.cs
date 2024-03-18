17164d5e4860832f84bee5eb74cda05dfe658ced
Doctor17-git <grom4322@gmail.com>
2024-03-18 12:39:40 +0300

    initial commit

---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 2;
+            double b = -11;
+            double c = 5;
+
+            var result = QuadraticEquation.Solve(a, b, c);
+
+            if (result.x1.HasValue && result.x2.HasValue)
+            {
+                Console.WriteLine($"Корни уравнения: x1 = {result.x1}, x2 = {result.x2}");
+            }
+            else if (result.x1.HasValue)
+            {
+                Console.WriteLine($"Уравнение имеет один корень: x = {result.x1}");
+            }
+            else
+            {
+                Console.WriteLine("Уравнение не имеет корней.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuadraticEquation/QuadraticEquation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEquation
+{
+    public class QuadraticEquation
+    {
+        public static (double? x1, double? x2) Solve(double a, double b, double c)
+        {
+            double discriminant = b * b - 4 * a * c;
+            if (discriminant < 0)
+            {
+                return (null, null); // Корней нет
+            }
+            else if (discriminant == 0)
+            {
+                double x = -b / (2 * a);
+                return (x, null); // Один корень
+            }
+            else
+            {
+                double sqrtDiscriminant = Math.Sqrt(discriminant);
+                double x1 = (-b + sqrtDiscriminant) / (2 * a);
+                double x2 = (-b - sqrtDiscriminant) / (2 * a);
+                return (x1, x2); // Два корня
+            }
+        }
+    }
+}
